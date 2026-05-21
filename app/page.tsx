209886621a876184d981b32aef249b39f00fb278
Erik Peteri <peteri.erik@gmail.com>
2026-05-21 21:15:24 +0300

    read books from a json file and display them on screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import { promises as fs } from "fs";
 
-export default function Home() {
+type Book = {
+  id: string,
+  title: string,
+  author: string,
+  published: number
+}
+
+export default async function Page() {
+  const file = await fs.readFile(process.cwd() + "/data/books.json", "utf8")
+  const data: Book[] = JSON.parse(file);
+  console.log(data)
   return (
     <div>
       <main>
-        Hello world
+        {data.map(book => (
+          <div key={book.id}>
+            <h3>{book.title}</h3>
+            <h4>{book.author}</h4>
+            <span>{book.published}</span>
+          </div>
+        ))}
       </main>
     </div>
   );
