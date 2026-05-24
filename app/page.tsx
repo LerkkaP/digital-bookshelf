fc50009363178e8a152e89d9dd68fe63f9651f80
Erik Peteri <peteri.erik@gmail.com>
2026-05-24 20:45:14 +0300

    improve visuals

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,15 @@ type Book = {
 export default async function Page() {
   const file = await fs.readFile(process.cwd() + "/data/books.json", "utf8")
   const data: Book[] = JSON.parse(file);
-  console.log(data)
+
   return (
     <div>
       <main className="grid-container">
         {data.map(book => (
           <div className="grid-item" key={book.id}>
-            <h3>{book.title}</h3>
-            <h4>{book.author}</h4>
-            <span>{book.published}</span>
+            <h2 className="book-title">{book.title}</h2>
+            <h3 className="book-author">{book.author}</h3>
+            <span className="book-published">{book.published}</span>
           </div>
         ))}
       </main>
